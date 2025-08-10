dba5d4bacf9c074982f55f809019df25e7e6bdcd
principalwater <kuzmin.vv@yahoo.com>
2025-08-10 23:55:57 +0300

    Switched from sed to variable checking

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -21,7 +21,7 @@ provider "docker" {}
 
 provider "docker" {
   alias = "remote_host"
-  host  = "ssh://${var.remote_ssh_user}@${var.remote_host_name}"
+  host  = var.storage_type == "local_storage" ? "unix:///var/run/docker.sock" : "ssh://${var.remote_ssh_user}@${var.remote_host_name}"
 }
 
 provider "aws" {
